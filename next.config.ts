c668e6c2f36edd8c37f9442952c905868624dc39
haha2109 <daothuhang2109@gmail.com>
2025-03-11 08:29:24 +0000

    feat: fuctions of invoices page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: "incremental",
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
